c362073860fbbc6aadbe2c408ea7630dbdf52902
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-09-02 23:23:20 +0530

    displayed posts, added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(response => setPosts(response))
+  }, []);
+
   return (
     <div>
-      <h2>Hello world</h2>
+      <h2>Posts from JSONplaceholder</h2>
+      <div>
+        {
+          (posts) ?
+          (<ul className='ul-flex'>
+            {posts.map((post) => {
+              return (<div>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>)
+            })
+            }
+          </ul>) : ( <h3>Loading...</h3> )
+        }
+      </div>
     </div>
   );
 }
